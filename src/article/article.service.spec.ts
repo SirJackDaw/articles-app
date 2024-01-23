73b69a2304d8888af36edf86fd63c79a7c2ccf43
Dmitry Zarubin <sirjackdaw@outlook.com>
2024-01-23 20:23:26 +0700

    article service unit test

--- src/article/article.service.spec.ts
+++ src/article/article.service.spec.ts
@@ -6,11 +6,11 @@ describe ('ArticleService', () => {
     let service: ArticleService;
 
     const mockArticleRepository = {
-        findMany: jest.fn(query => ([])),
-        findById: jest.fn(id => ({})),
+        findMany: jest.fn(query => (Promise.resolve([]))),
+        findById: jest.fn(id => (Promise.resolve({id: id}))),
         create: jest.fn(dto => (Promise.resolve({ id: '1', ...dto, author: { id: dto.author } }))),
         updateWithUser: jest.fn((id, userId, dto) => (Promise.resolve({ id: id, author: userId, ...dto }))),
-        deleteWithUser: jest.fn((id, userId) => ({})),
+        deleteWithUser: jest.fn((id, userId) => (Promise.resolve())),
     }
 
     beforeEach(async () => {
@@ -35,5 +35,32 @@ describe ('ArticleService', () => {
             ...dto,
             author: { id: '1' }
         })
+
+        expect(mockArticleRepository.create).toHaveBeenCalledWith(dto)
+    });
+
+    it ('should delete article', async () => {
+        await expect(service.delete('1', '1')).resolves.toBeUndefined();
+        await expect(mockArticleRepository.deleteWithUser).toHaveBeenCalledWith('1', '1')
+    })
+
+    it ('should update article', async () => {
+        const dto = { title: 'title', description: 'description', author: '1'}
+        expect(service.update('1', '1', dto)).resolves.toEqual({ 
+            id: '1', 
+            ...dto,	
+        })
+        expect(mockArticleRepository.updateWithUser).toHaveBeenCalledWith('1', '1', dto)
+    })
+
+    it ('should find articles', async () => {
+        const query = { page: '1', perPage: '10', authorName: 'name', dateFrom: '2020-01-01', dateTo: '2020-01-02', timezone: '0' }
+        expect(service.findArticles(query)).resolves.toEqual([])
+        expect(mockArticleRepository.findMany).toHaveBeenCalledWith(query)
+    })
+
+    it ('should find article by id', async () => {
+        expect(service.findById('1')).resolves.toEqual({ id: '1' })
+        expect(mockArticleRepository.findById).toHaveBeenCalledWith('1')
     })
 });
